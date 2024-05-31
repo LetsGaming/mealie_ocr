2d5e9dc630a3185112a3ae543ac0ac29197c1719
kirchner <kirchner@visionconnect.de>
2024-06-01 00:27:07 +0200

    first commit

---
+++ mealie_ocr.py
@@ -0,0 +1,96 @@
+import os
+import json
+import pytesseract
+from PIL import Image
+import openai
+import asyncio
+import aiohttp
+from dotenv import load_dotenv
+
+from utils import recipe_data
+
+# Load environment variables
+load_dotenv()
+
+# Mealie service URL and API key
+mealie_url = os.getenv('MEALIE_URL')
+mealie_api_key = os.getenv('MEALIE_API_KEY')
+
+# OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def clean_special_characters(text):
+    # Replace special characters
+    cleaned_text = text.replace("�", "").replace("°C", "C").replace("ö", "oe").replace("ä", "ae").replace("ü", "ue").replace("ß", "ss").replace("```", "").replace("json", "")
+    return cleaned_text
+
+def extract_text_from_image(image_path: str) -> str:
+    try:
+        image = Image.open(image_path)
+        pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+        text = pytesseract.image_to_string(image)
+        return text
+    except Exception as e:
+        print(f"Error extracting text from image: {e}")
+        return ""
+
+def generate_recipe_from_text(text: str) -> dict:
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful Assistant, specialized on the selfhost service 'Mealie'."},
+                {"role": "user", "content": f"This is the JSON-Format for Mealie Recipes: {recipe_data}"},
+                {"role": "user", "content": f"Create a detailed Mealie recipe in JSON format using the following text. The response should contain only the JSON. The JSON should be directly importable using the Mealie API. For the ingredients and instructions, use ONLY this text:\n\n{text}"}
+            ],
+            max_tokens=2500
+        )
+        response_content = response['choices'][0]['message']['content'].strip()
+        cleaned_content = clean_special_characters(response_content)
+        recipe_json = json.loads(cleaned_content)
+        return recipe_json
+    except Exception as e:
+        print(f"Error generating recipe from text: {e}")
+        raise
+
+def create_mealie_recipe(recipe_data: dict) -> dict:
+    user_id = os.getenv('MEALIE_USER_ID')
+    group_id = os.getenv('MEALIE_GROUP_ID')
+    
+    recipe_data['userId'] = user_id
+    recipe_data['groupId'] = group_id
+    
+    return recipe_data
+
+async def upload_recipe_to_mealie(recipe: dict):
+    headers = {
+        'Authorization': f'Bearer {mealie_api_key}', 
+        'Content-Type': 'application/json'
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(mealie_url, json=recipe, headers=headers) as response:
+            if response.status == 201:
+                print("Recipe successfully uploaded.")
+            else:
+                print(f"Failed to upload recipe. Status code: {response.status}")
+                print(await response.text())
+
+async def main():
+    image_path = "C:/Users/Domenic/Desktop/IMG_6962.jpg"
+    
+    extracted_text = extract_text_from_image(image_path)
+
+    try:
+        generated_recipe = generate_recipe_from_text(extracted_text)
+        print(f"Generated Recipe: {generated_recipe}")
+    except Exception as e:
+        generated_recipe = None
+
+    if generated_recipe:
+        mealie_recipe = create_mealie_recipe(recipe_data=generated_recipe)
+        print(f"Mealie Recipe JSON: {mealie_recipe}")
+
+        await upload_recipe_to_mealie(mealie_recipe)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ utils.py
@@ -0,0 +1,148 @@
+recipe_data = {
+  "id": "bc329242-10be-4e61-a699-d02cfc666145",
+  "userId": "882cbfc1-4559-4c95-a912-53f22f87ae3a",
+  "groupId": "d8a0eeb4-64e9-411f-8478-3479ab3ed50a",
+  "name": "Zucchini-Quiche – einfach und sooo lecker!",
+  "slug": "zucchini-quiche-einfach-und-sooo-lecker",
+  "image": "Fugf",
+  "recipeYield": "4 servings",
+  "totalTime": "30 Minuten",
+  "prepTime": "30 Minuten",
+  "cookTime": "null",
+  "performTime": "none",
+  "description": "Tadaa – hier ist unsere absolut leckere Zucchini-Quiche mit Feta, Schmand und frischen Tomaten. Etwas Oregano im Teig sorgt für einen besonders feinen Boden!",
+  "recipeCategory": [],
+  "tags": [
+    {
+      "id": "063614da-961c-48ac-8886-0dc7c48e734f",
+      "name": "Kirschtomate",
+      "slug": "kirschtomate"
+    },
+    {
+      "id": "0e63847c-bc90-4f33-8ff8-f659801ca6a7",
+      "name": "Öl",
+      "slug": "ol"
+    }
+  ],
+  "tools": [],
+  "rating": "null",
+  "orgURL": "https://www.slowlyveggie.de/rezepte/zucchini-quiche-einfach-lecker",
+  "dateAdded": "2024-05-31",
+  "dateUpdated": "2024-05-31T11:51:52.471693",
+  "createdAt": "2024-05-31T00:25:38.650060",
+  "updateAt": "2024-05-31T11:51:52.475891",
+  "lastMade": "null",
+  "recipeIngredient": [
+    {
+      "quantity": "200",
+      "unit": {
+        "id": "fd74b2b7-77e0-4013-b395-6c4ebc788c5e",
+        "name": "Gramm",
+        "pluralName": "null",
+        "description": "",
+        "extras": {},
+        "fraction": "true",
+        "abbreviation": "g",
+        "pluralAbbreviation": "",
+        "useAbbreviation": "false",
+        "aliases": [],
+        "createdAt": "2024-05-29T00:23:23.158829",
+        "updateAt": "2024-05-29T00:23:23.158835"
+      },
+      "food": "null",
+      "note": "Mehl (Typ 405)",
+      "isFood": "false",
+      "disableAmount": "true",
+      "display": "200 g Mehl (Typ 405)",
+      "title": "null",
+      "originalText": "null",
+      "referenceId": "5e99674c-f553-4411-9311-6379eedcd01c"
+    },
+    {
+      "quantity": "0.5",
+      "unit": {
+        "id": "a133e932-c176-41ab-9b3e-eb55322de62a",
+        "name": "Teelöffel",
+        "pluralName": "null",
+        "description": "",
+        "extras": {},
+        "fraction": "true",
+        "abbreviation": "TL",
+        "pluralAbbreviation": "",
+        "useAbbreviation": "false",
+        "aliases": [],
+        "createdAt": "2024-05-29T00:23:22.985077",
+        "updateAt": "2024-05-29T00:23:22.985083"
+      },
+      "food": "null",
+      "note": "Plus Mehl zum Ausrollen",
+      "isFood": "false",
+      "disableAmount": "true",
+      "display": "Plus Mehl zum Ausrollen",
+      "title": "null",
+      "originalText": "null",
+      "referenceId": "5ce729bc-9d8b-4094-9bda-5b12ef73c892"
+    },
+    {
+      "quantity": "2",
+      "unit": {
+        "id": "a133e932-c176-41ab-9b3e-eb55322de62a",
+        "name": "Teelöffel",
+        "pluralName": "null",
+        "description": "",
+        "extras": {},
+        "fraction": "true",
+        "abbreviation": "TL",
+        "pluralAbbreviation": "",
+        "useAbbreviation": "false",
+        "aliases": [],
+        "createdAt": "2024-05-29T00:23:22.985077",
+        "updateAt": "2024-05-29T00:23:22.985083"
+      },
+      "food": "null",
+      "note": "Salz",
+      "isFood": "false",
+      "disableAmount": "true",
+      "display": "0.5 TL Salz",
+      "title": "null",
+      "originalText": "null",
+      "referenceId": "3f594051-a150-44fe-82d8-4a0352128b77"
+    }
+  ],
+  "recipeInstructions": [
+    {
+      "id": "99cbb6b1-f1c8-4667-962f-1b69d19dfbb6",
+      "title": "",
+      "text": "1. Für den Teig Mehl Salz Oregano Butter und Ei in eine Schüssel geben und zu einem glatten Teig verkneten. In Folie eingewickelt für 30 Minuten kaltstellen. Backofen auf 180 Grad (Ober-/Unterhitze) vorheizen.",
+      "ingredientReferences": []
+    },
+    {
+      "id": "b60b9710-3c73-4361-95d0-7445c0aa4286",
+      "title": "",
+      "text": "2. Zucchini waschen halbieren und in Scheiben schneiden. Knoblauch abziehen und fein schneiden. Kirschtomaten halbieren. Öl in einer Pfanne erhitzen und Zucchini bei mittlerer Hitze für 5 Minuten anbraten bis sie sich goldbraun verfärbt. Knoblauch dazugeben und für 1 Minute mitdünsten. Die Mischung mit einer Prise Salz würzen. Pfanne vom Herd nehmen.",
+      "ingredientReferences": []
+    }
+  ],
+  "nutrition": {
+    "calories": "null",
+    "fatContent": "null",
+    "proteinContent": "null",
+    "carbohydrateContent": "null",
+    "fiberContent": "null",
+    "sodiumContent": "null",
+    "sugarContent": "null"
+  },
+  "settings": {
+    "public": "true",
+    "showNutrition": "true",
+    "showAssets": "false",
+    "landscapeView": "false",
+    "disableComments": "false",
+    "disableAmount": "false",
+    "locked": "false"
+  },
+  "assets": [],
+  "notes": [],
+  "extras": {},
+  "comments": []
+}
